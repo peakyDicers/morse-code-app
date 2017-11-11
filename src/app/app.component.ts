4827fa5b3527cdffe24ca756de58018e0f395109
Ian Chui <ian.chui@hotmail.com>
2017-11-10 22:29:49 -0500

    added howler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'the Official English to Morse Code Translator';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { InputComponent } from './components/input/input.component';
+import { DisplayComponent } from './components/display/display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputComponent
+    InputComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule
